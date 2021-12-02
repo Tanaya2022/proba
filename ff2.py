338a0609dfc4eda30df74b4e1a0d2f05e9dd641a
Учитель <anisina@yandex.ru>
2021-12-02 19:10:32 +0500

    function

---
+++ ff2.py
@@ -0,0 +1,13 @@
+def calc(sign, a, b):
+
+    if sign == '+':
+        return a + b
+    if sign == '-':
+        return a - b
+    if sign == '*':
+        return a * b
+    if sign == '/':
+        if b!=0:
+        return a / b
+
+
